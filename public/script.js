d87a26c6218b26631b735d2b977ff943516648ed
henriquefirmino <henriquecasarini4@gmail.com>
2021-10-21 15:04:04 -0300

    tentando fazer o socket funcionar

--- public/script.js
+++ public/script.js
@@ -1,7 +1,9 @@
-let canvas = document.getElementById('canvas')
+let canvas = document.getElementById("canvas");
 
 canvas.width = 0.98 * window.innerWidth;
 canvas.height = window.innerHeight;
+
+var io = io.connect("http://localhost:8080");
 
 let ctx = canvas.getContext("2d");
 
@@ -10,19 +12,26 @@ let y;
 let mouseDown = false;
 
 window.onmousedown = (e) => {
+    ctx.moveTo(x, y);
     mouseDown = true;
-}
+};
 
 window.onmouseup = (e) => {
     mouseDown = false;
-}
+};
+
+io.on('ondraw', ({x,y}) => {
+    ctx.lineTo(x,y);
+    ctx.stroke();
+});
 
 window.onmousemove = (e) => {
     x = e.clientX;
     y = e.clientY;
     
     if (mouseDown) {
+        io.emit("draw", {x,y});
         ctx.lineTo(x , y);
         ctx.stroke();
     }
-}
+};
